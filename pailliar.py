547a8c2f41bf843424dbedbb7ab2e9e625873002
weiqiy93 <94254217+weiqiy93@users.noreply.github.com>
2021-12-03 01:36:58 -0500

    Update pailliar.py

--- pailliar.py
+++ pailliar.py
@@ -1,82 +1,90 @@
-import random
-from math import pow
-import gmpy2 as gy
-import time
+import gmpy2 
+import math
+from random import randint
+from math import gcd
 import libnum
-
+rs = gmpy2.random_state()
 
 
 class Paillier(object):
-
-    def __init__(self, pubKey=None, priKey=None):
-        self.pubKey = pubKey
-        self.priKey = priKey
-
-    def __gen_prime__(self, rs):
-        p = gy.mpz_urandomb(rs, 1024)
-        while not gy.is_prime(p):
-            p += 1
+    
+ 
+    def __init__(self):
+        
+        self.publicKey = None
+        self.privateKey = None
+        
+    def lcm(self, x, y):
+        
+        return x * y // gcd(x, y) 
+    
+    def sTn(self,m):
+        arr = bytes(m, 'utf-8')
+        m = int.from_bytes(arr, 'big')
+        return m
+          
+     
+    def getP(self):
+        
+        p = gmpy2.mpz_urandomb(rs, 2024)
+        while not gmpy2.is_prime(p):
+            p = p + 1
         return p
+    
+    def getKeys(self):
+     
+        p = self.getP()
+        q = self.getP()
+        
+        n = p * q
+        lamda = self.lcm(p-1, q-1)
+        g = randint(0,1024)
+        mu = (pow(g,lamda,n*n)-1) // n
+        mu = libnum.invmod(mu, n)
+        
+        self.publicKey = [n, g]
+        self.privateKey = [lamda, mu]
+    
+    def encrypt(self, m):
+        
+        m = self.sTn(m)
+        # print(m)
+        
+        n,g = self.publicKey
+        r = randint(0,n)
+        c = (pow(g, m, n*n) * pow(r, n, n*n)) % (n*n)
+        
+        # print(c)
+        return c
+    
+    def decrypt(self,c):
+        
+        n, g = self.publicKey
+        lamda, mu = self.privateKey
+        
+        c = (pow(c,lamda, n*n) - 1) // n * mu
+        c = c % n
+        c = int (c)
+        # print(c)
+        
+        m = libnum.n2s(c)
+        m = m.decode("utf-8") 
+        return m
 
-    def __div__(self, x, n):
-        res = gy.div((x - 1), n)
-        return res
 
-    def __key_gen__(self):
-        # generate random state
-        while True:
-
-            rs = gy.random_state(int(time.time()))
-            p = self.__gen_prime__(rs)
-            q = self.__gen_prime__(rs)
-            n = p * q
-            lmd = (p - 1) * (q - 1)
-
-            if gy.gcd(n, lmd) == 1:
-                break
-
-        g = n + 1
-        mu = gy.invert(lmd, n)
-
-        self.pubKey = [n, g]
-        self.priKey = [lmd, mu]
-        return
-
-    def decrypt(self, ciphertext):
-        n, g = self.pubKey
-        lmd, mu = self.priKey
-        m = self.__div__(gy.powmod(ciphertext, lmd, n ** 2), n) * mu % n
-        print("raw message:", m)
-        plaintext = libnum.n2s(int(m))
-        return plaintext
-
-    def encrypt(self, plaintext):
-        m = libnum.s2n(plaintext)
-        n, g = self.pubKey
-        r = gy.mpz_random(gy.random_state(int(time.time())), n)
-        while gy.gcd(n, r) != 1:
-            r += 1
-        ciphertext = gy.powmod(g, m, n ** 2) * gy.powmod(r, n, n ** 2) % (n ** 2)
-        return ciphertext
-
-
-if __name__ == "__main__":
-    x = 2
-    y = 3
-
-    string = "make american great again"
-
-    byte = bytes(string, 'utf-8')
-
+    
+if __name__ == "__main__":    
+    string = 'CWRo get ae.'
+    byte = ''
+    text = string #byte
+    
     p = Paillier()
-    p.__key_gen__()
-    # Key = p.pubKey
-
-    plaintext = byte
-    print("Plaintext:", plaintext)
-
-    ciphertext = p.encrypt(plaintext)
-    print("Ciphertext:", ciphertext)
-
-    deciphertext = p.decrypt(ciphertext)
-    print("Deciphertext: ", deciphertext)
+    p.getKeys()
+    
+    
+    c = p.encrypt(text) 
+    m = p.decrypt(c)
+    
+    print("Plaintext:", text)
+    print("Ciphertext:", c)
+    print("Deciphertext: ", m)
