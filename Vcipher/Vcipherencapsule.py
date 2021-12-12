34723172dc5a5d293450ff9ad03080167bc6432a
Haolai Che <hxc859@case.edu>
2021-12-12 02:12:49 -0500

    harder encrypted

--- Vcipher/Vcipherencapsule.py
+++ Vcipher/Vcipherencapsule.py
@@ -22,9 +22,9 @@ class Vigenere():
 
     def __init__(self):
         self.asciilist=[' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', 
-                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', 
-                        '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
-                        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', 
+                        '0', 'A', '2', 'C', '4', 'E', '6', '7', '8', 'X', ':', 'K', '<', '=', '>', '?', 
+                        '@', '1', 'B', '3', 'D', '5', 'F', 'G', 'H', 'I', 'J', ';', 'L', 'M', 'N', 'O', 
+                        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', '9', 'Y', 'Z', '[', '\\', ']', '^', '_', 
                         '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
                         'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~','\n']
         
@@ -50,12 +50,17 @@ class Vigenere():
     def cipher_Text(self, data_file, key):
         cipher_text = ""
         cipher_text_path = "Ciphertext.txt"
+        start = time.time()
         with open(data_file, 'r') as f:
             string = f.read()
         for i in range(len(string)):
             if string[i] not in self.asciilist:
                 cipher_text += string[i]
             cipher_text += self.asciilist[(self.asciilist.index(string[i])+self.asciilist.index(key[i%len(key)]))%len(self.asciilist)]
+        
+        end = time.time()
+        print("encrypt text time",end-start)
+
 
         with open(cipher_text_path,'w') as f:
             f.write(cipher_text)
@@ -67,12 +72,15 @@ class Vigenere():
         output = ""
         cipher_text_path = "Ciphertext.txt"
         PlainText_path = "Plaintext.txt"
+        start = time.time()
         with open(cipher_text_path, 'r') as f:
             cipher_text = f.read()
         for i in range(len(cipher_text)):
             if cipher_text[i] not in self.asciilist:
                 output += cipher_text[i]
             output += self.asciilist[(self.asciilist.index(cipher_text[i]) - self.asciilist.index(key[i % len(key)])) % len(self.asciilist)]
+        end = time.time()
+        print("decrypt text time",end-start)
         with open(PlainText_path, 'w') as f:
             f.write(output)
         return output
@@ -82,12 +90,15 @@ class Vigenere():
     def encrypt_img(self, img_path, key):
         byte_path = 'image_bytes.txt'
         cipher_path = 'cipher_img.txt'
+        start = time.time()
         with open(img_path, 'rb') as f :
             img_byte2string = base64.b64encode(f.read())
             img_string = img_byte2string.decode('ascii')
         with open(byte_path, 'w') as f:
             f.write(img_string)
         Cipher_Image = self.encryptstring(img_string,key )
+        end = time.time()
+        print("Encrypt image time:",end-start)
         with open(cipher_path, 'w') as f:
             f.write(Cipher_Image)
         
@@ -95,12 +106,15 @@ class Vigenere():
         cipher_path = 'cipher_img.txt'
         plainimg_path = 'PlainImage.txt'
         originalimg_path = 'originalImage.jpg'
+        start = time.time()
         with open(cipher_path, 'r') as f:
             Cipher_Image = f.read()
             Plain_Image = self.decryptstring(Cipher_Image,key)
         with open(plainimg_path, "w") as p:
             p.write(Plain_Image)
         String2Bytes = Plain_Image.encode('ascii')
+        end = time.time()
+        print("Decrypt image time", end-start)
 
         with open(originalimg_path,'wb') as str2image:
             str2image.write(base64.b64decode(String2Bytes))
@@ -110,7 +124,7 @@ def main():
     Vc = Vigenere()
     with open('key1.txt','r') as f:
         key = f.read()
-    file_path = 'plaintext4.txt'
+    file_path = 'pppp5.txt'
     img_path = "figure3_1920_970.png"
     Vc.cipher_Text(file_path,key)
     Vc.decrypt_text(key)
